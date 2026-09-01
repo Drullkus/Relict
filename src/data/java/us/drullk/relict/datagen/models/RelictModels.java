8e5bf56a45d1c666a289f5401a69647f24e7e63a
Drullkus <5010174+Drullkus@users.noreply.github.com>
2026-09-01 02:16:05 -0700

    Custom Mars Portal texture and update ruin

--- src/data/java/us/drullk/relict/datagen/models/RelictModels.java
+++ src/data/java/us/drullk/relict/datagen/models/RelictModels.java
@@ -6,20 +6,22 @@ import net.minecraft.client.data.models.ModelProvider;
 import net.minecraft.client.data.models.blockstates.MultiVariantGenerator;
 import net.minecraft.client.data.models.blockstates.PropertyDispatch;
 import net.minecraft.client.data.models.model.ItemModelUtils;
-import net.minecraft.client.data.models.model.ModelLocationUtils;
+import net.minecraft.client.data.models.model.ModelTemplate;
 import net.minecraft.client.data.models.model.ModelTemplates;
 import net.minecraft.client.data.models.model.TextureMapping;
+import net.minecraft.client.data.models.model.TextureSlot;
 import net.minecraft.client.renderer.block.dispatch.Variant;
 import net.minecraft.client.resources.model.sprite.Material;
 import net.minecraft.core.Direction;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.Identifier;
-import net.minecraft.world.level.block.Blocks;
 import us.drullk.relict.Relict;
 import us.drullk.relict.block.RelictPortalBlock;
 import us.drullk.relict.datagen.cipherchest.CipherChestModelGenerator;
 import us.drullk.relict.init.RelictBlocks;
 import us.drullk.relict.init.RelictItems;
+
+import java.util.Optional;
 
 /**
  * Client-side model/blockstate/item-model datagen entry point. Thin by design: each feature area owns its
@@ -28,6 +30,11 @@ import us.drullk.relict.init.RelictItems;
  * provider is handed.
  */
 public class RelictModels extends ModelProvider {
+
+    private static final ModelTemplate MARS_PORTAL_NS_TEMPLATE =
+            new ModelTemplate(Optional.of(Relict.id("block/template_mars_portal_ns")), Optional.of("_ns"), TextureSlot.ALL);
+    private static final ModelTemplate MARS_PORTAL_EW_TEMPLATE =
+            new ModelTemplate(Optional.of(Relict.id("block/template_mars_portal_ew")), Optional.of("_ew"), TextureSlot.ALL);
 
     public RelictModels(PackOutput output) {
         super(output, Relict.MODID);
@@ -53,13 +60,16 @@ public class RelictModels extends ModelProvider {
         CipherChestModelGenerator.bootstrap(blockModels, itemModels);
     }
 
-    // FIXME replace nether_portal placeholder texture for custom
     private static void registerMarsPortal(BlockModelGenerators blockModels) {
+        TextureMapping textures = TextureMapping.cube(new Material(Relict.id("block/mars_portal")));
+        Identifier nsModel = MARS_PORTAL_NS_TEMPLATE.create(RelictBlocks.MARS_PORTAL.get(), textures, blockModels.modelOutput);
+        Identifier ewModel = MARS_PORTAL_EW_TEMPLATE.create(RelictBlocks.MARS_PORTAL.get(), textures, blockModels.modelOutput);
+
         blockModels.blockStateOutput.accept(
                 MultiVariantGenerator.dispatch(RelictBlocks.MARS_PORTAL.get())
                         .with(PropertyDispatch.initial(RelictPortalBlock.AXIS)
-                                .select(Direction.Axis.X, BlockModelGenerators.plainVariant(ModelLocationUtils.getModelLocation(Blocks.NETHER_PORTAL, "_ns")))
-                                .select(Direction.Axis.Z, BlockModelGenerators.plainVariant(ModelLocationUtils.getModelLocation(Blocks.NETHER_PORTAL, "_ew")))));
+                                .select(Direction.Axis.X, BlockModelGenerators.plainVariant(nsModel))
+                                .select(Direction.Axis.Z, BlockModelGenerators.plainVariant(ewModel))));
     }
 
     private static void registerBasaltSand(BlockModelGenerators blockModels, ItemModelGenerators itemModels) {
